6520c26d34c39ca081a209c3e76bbeea0f24284a
José Manuel Carrichi Chávez <josemanuelcarrichi16@gmail.com>
2024-03-22 20:42:34 -0600

    comandos con modificadores

---
+++ ejecutarTareasAuto.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_LINE_LENGTH 100
+
+int main() {
+    FILE *archivo;
+    char linea[MAX_LINE_LENGTH];
+
+    archivo = fopen("tareas.txt", "r");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo");
+        exit(1);
+    }
+
+    while (fgets(linea, MAX_LINE_LENGTH, archivo) != NULL) {
+        // Eliminar el salto de línea al final de la línea
+        linea[strcspn(linea, "\n")] = 0;
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(1);
+        }
+
+        if (pid == 0) {
+            // Proceso hijo
+            char* args[] = { "sh", "-c", linea, NULL };
+            if (execvp("sh", args) < 0) {
+                perror("exec");
+                exit(1);
+            }
+        } else {
+            // Proceso padre
+            wait(NULL); // Esperar a que el proceso hijo termine
+        }
+    }
+
+    fclose(archivo);
+    return 0;
+}
